6ace5900186f7c5fa7b946d22eb22e28e5ca3b3a
Spoorloos <59575072+Spoorloos@users.noreply.github.com>
2022-06-27 13:15:35 +0200

    Update universal_esp.lua

--- universal_esp.lua
+++ universal_esp.lua
@@ -6,10 +6,10 @@ local players = game:GetService("Players");
 local workspace = game:GetService("Workspace");
 
 -- variables
-local camera = workspace.CurrentCamera;
 local get_pivot = workspace.GetPivot;
-local wtvp = camera.WorldToViewportPoint;
+local camera = workspace.CurrentCamera;
 local viewport_size = camera.ViewportSize;
+local wtvp = camera.WorldToViewportPoint;
 local localplayer = players.LocalPlayer;
 local cache = {};
 
@@ -65,36 +65,36 @@ local function update_esp()
             local position, visible = wtvp(camera, cframe.Position);
 
             esp.box.Visible = visible;
+            esp.tracer.Visible = visible;
             esp.name.Visible = visible;
             esp.distance.Visible = visible;
-            esp.tracer.Visible = visible;
 
             if visible then
-                local scale_factor = 1 / (position.Z * tan(rad(camera.FieldOfView * 0.5)) * 2) * 1000;
-                local width, height = floor(3 * scale_factor), floor(5 * scale_factor);
+                local scale_factor = 1 / (position.Z * tan(rad(camera.FieldOfView * 0.5)) * 2) * 100;
+                local width, height = floor(30 * scale_factor), floor(50 * scale_factor);
                 local x, y = floor(position.X), floor(position.Y);
 
                 esp.box.Size = new_vector2(width, height);
                 esp.box.Position = new_vector2(floor(x - width * 0.5), floor(y - height * 0.5));
                 esp.box.Color = player.TeamColor.Color;
 
-                esp.name.Text = player.Name;
-                esp.name.Position = new_vector2(x, floor(y - height * 0.5 - esp.name.TextBounds.Y - 2));
-                esp.name.Color = player.TeamColor.Color;
-
-                esp.distance.Text = floor(position.Z) .. " studs";
-                esp.distance.Position = new_vector2(x, floor(y + height * 0.5 + 2));
-                esp.distance.Color = player.TeamColor.Color;
-
                 esp.tracer.From = new_vector2(floor(viewport_size.X * 0.5), floor(viewport_size.Y));
                 esp.tracer.To = new_vector2(x, floor(y + height * 0.5));
                 esp.tracer.Color = player.TeamColor.Color;
+
+                esp.name.Text = player.Name;
+                esp.name.Position = new_vector2(x, floor(y - height * 0.5 - esp.name.TextBounds.Y) - 2);
+                esp.name.Color = player.TeamColor.Color;
+
+                esp.distance.Text = floor(position.Z) .. " studs";
+                esp.distance.Position = new_vector2(x, floor(y + height * 0.5) + 2);
+                esp.distance.Color = player.TeamColor.Color;
             end
         else
             esp.box.Visible = false;
+            esp.tracer.Visible = false;
             esp.name.Visible = false;
             esp.distance.Visible = false;
-            esp.tracer.Visible = false;
         end
     end
 end
