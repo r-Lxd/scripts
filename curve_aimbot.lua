6748e1342245f9ec5e13d505f15d05bee9bc4de4
Spoorloos <59575072+Spoorloos@users.noreply.github.com>
2022-07-14 13:22:29 +0200

    Update curve_aimbot.lua

--- curve_aimbot.lua
+++ curve_aimbot.lua
@@ -11,8 +11,8 @@ local camera = workspace.CurrentCamera;
 local wtvp = camera.WorldToViewportPoint;
 local localplayer = players.LocalPlayer;
 local mouse_pos = inputservice.GetMouseLocation;
+local mouse_downs = inputservice.GetMouseButtonsPressed;
 local curve = { player = nil, i = 0 };
-local mouse_down = false;
 
 -- functions
 local function get_closest()
@@ -34,39 +34,37 @@ local function get_closest()
     return player, position;
 end
 
-local function quad_bezier(t, p0, p1)
-    local o0 = p0 + (p1 - p0) * Vector2.new(0.5, 0);
-    return (1 - t)^2 * p0 + 2 * (1 - t) * t * o0 + t^2 * p1;
+local function is_mouse_down(input_type)
+    for _, input in next, mouse_downs(inputservice) do
+        if input.UserInputType == input_type then
+            return true;
+        end
+    end
+    return false;
+end
+
+local function quad_bezier(t, p0, p1, o0)
+    return (1 - t)^2 * p0 + 2 * (1 - t) * t * (p0 + (p1 - p0) * o0) + t^2 * p1;
 end
 
 -- connections
-inputservice.InputBegan:Connect(function(input)
-    if input.UserInputType == Enum.UserInputType.MouseButton2 then
-        mouse_down = true;
-    end
-end);
-
-inputservice.InputEnded:Connect(function(input)
-    if input.UserInputType == Enum.UserInputType.MouseButton2 then
-        mouse_down = false;
-    end
-end);
-
 runservice.Heartbeat:Connect(function()
-    if mouse_down then
+    if is_mouse_down(Enum.UserInputType.MouseButton2) then
         local player, screen = get_closest();
         if player and player.Character then
             if curve.player ~= player or curve.i > 1 then
-                curve = { player = player, i = 0 };
+                curve.player = player;
+                curve.i = 0;
             end
 
             local mouse = mouse_pos(inputservice);
-            local delta = quad_bezier(curve.i, mouse, screen) - mouse;
+            local delta = quad_bezier(curve.i, mouse, screen, Vector2.new(0.5, 0)) - mouse;
             mousemoverel(delta.X, delta.Y);
 
             curve.i += 0.025;
         end
     else
-        curve = { player = nil, i = 0 };
+        curve.player = nil;
+        curve.i = 0;
     end
 end);
