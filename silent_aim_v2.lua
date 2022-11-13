8441bce790b82678ff6f067917147be842c42b77
mickey <59575072+Spoorloos@users.noreply.github.com>
2022-11-13 21:24:50 +0100

    Update silent_aim_v2.lua

--- silent_aim_v2.lua
+++ silent_aim_v2.lua
@@ -73,7 +73,7 @@ old = hookfunction(particle.new, function(args)
         if position and entry then
             local bulletSpeed = args.velocity.Magnitude;
             local travelTime = (position - args.position).Magnitude / bulletSpeed;
-            local idx = table.find(debug.getstack(2), args.velocity);
+            local index = table.find(debug.getstack(2), args.velocity);
 
             args.velocity = physics.trajectory(
                 args.position,
@@ -81,7 +81,7 @@ old = hookfunction(particle.new, function(args)
                 position + entry._velspring.p * travelTime,
                 bulletSpeed);
 
-            debug.setstack(2, idx, args.velocity);
+            debug.setstack(2, index, args.velocity);
         end
     end
     return old(args);
