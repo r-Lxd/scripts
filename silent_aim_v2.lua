534d9e5bdaecb6fe6890e5e5c9f0c08155a64bce
mickey <59575072+Spoorloos@users.noreply.github.com>
2022-11-14 19:52:14 +0100

    Update silent_aim_v2.lua

--- silent_aim_v2.lua
+++ silent_aim_v2.lua
@@ -66,7 +66,8 @@ local function trajectory(dir, velocity, accel, speed)
         -accel:Dot(velocity),
         accel:Dot(dir) + velocity:Dot(velocity) - speed*speed,
         velocity:Dot(dir) * 2,
-        dir:Dot(dir))};
+        dir:Dot(dir)
+    )};
 
     for _, t in next, roots do
         if t and t > 0 then
