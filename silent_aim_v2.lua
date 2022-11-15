b5fb179acb0a7fa7a7aedd039de16dcf3ff57c40
mickey <59575072+Spoorloos@users.noreply.github.com>
2022-11-15 14:21:42 +0100

    Update silent_aim_v2.lua

--- silent_aim_v2.lua
+++ silent_aim_v2.lua
@@ -1,6 +1,5 @@
 -- phantom forces silent aim
--- by mickey#3373, updated 11/14/22
--- credits: integer, wyvern
+-- by mickey#3373, credits to eev#1993
 -- https://v3rmillion.net/showthread.php?tid=1193218
 
 -- variables
@@ -21,7 +20,7 @@ local function isVisible(position, ignore)
 end
 
 local function getClosest(dir, ignore)
-    local _product = 1 - (fov or 180) / 90;
+    local _angle = fov or math.huge;
     local _position, _entry;
 
     replication.operateOnAllEntries(function(player, entry)
@@ -34,8 +33,9 @@ local function getClosest(dir, ignore)
 
             if not (visibleCheck and not isVisible(part.Position, ignore)) then
                 local product = dir.Unit:Dot((part.Position - camera.CFrame.p).Unit);
-                if product > _product then
-                    _product = product;
+                local angle = math.deg(math.acos(product));
+                if angle < _angle then
+                    _angle = angle;
                     _position = part.Position;
                     _entry = entry;
                 end
