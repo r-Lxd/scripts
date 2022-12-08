5cfe89d58eaa64e86da2343e32f4b1a5d396fac0
mickey <59575072+Spoorloos@users.noreply.github.com>
2022-12-08 21:07:33 +0100

    Update pf-actor-bypass.lua

--- pf-actor-bypass.lua
+++ pf-actor-bypass.lua
@@ -6,24 +6,27 @@ if placeId == 292439477 or placeId == 299659045 then
 
     -- connect parallel bypass
     local old; 
-    old = hookmetamethod(runService.Heartbeat, "__index", function(_, index)
-        return old(_, (index == "ConnectParallel" and not checkcaller()) and "Connect" or index);
+    old = hookmetamethod(runService.Heartbeat, "__index", function(self, index)
+        if index == "ConnectParallel" and not checkcaller() then
+            index = "Connect";
+        end
+        return old(self, index);
     end);
 
     -- actor bypass
     replicatedFirst.ChildAdded:Connect(function(instance)
-       if instance:IsA("Actor") then
-           replicatedFirst.ChildAdded:Wait();
-     
-           local container = Instance.new("Folder");
-           for _, child in next, instance:GetChildren() do
-               child.Parent = container;
-           end
-     
-           container.Name = instance.Name;
-           container.Parent = instance.Parent;
-     
-           instance.Parent = nil;
-       end
+        if instance:IsA("Actor") then
+            replicatedFirst.ChildAdded:Wait();
+
+            local container = Instance.new("Folder");
+            container.Name = instance.Name;
+            container.Parent = instance.Parent;
+
+            for _, child in next, instance:GetChildren() do
+                child.Parent = container;
+            end
+
+            instance.Parent = nil;
+        end
     end);
 end
