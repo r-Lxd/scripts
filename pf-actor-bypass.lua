c462834ea9ebd52ee3b2294e9b9de148a131e130
mickey <59575072+Spoorloos@users.noreply.github.com>
2022-12-08 20:17:40 +0100

    Update pf-actor-bypass.lua

--- pf-actor-bypass.lua
+++ pf-actor-bypass.lua
@@ -1,23 +1,26 @@
 local runService = game:GetService("RunService");
 local replicatedFirst = game:GetService("ReplicatedFirst");
 
-local old; 
-old = hookmetamethod(runService.Heartbeat, "__index", function(_, index)
-	return old(_, index == "ConnectParallel" and "Connect" or index);
-end);
-	
-replicatedFirst.ChildAdded:Connect(function(instance)
-   if instance:IsA("Actor") then
-       replicatedFirst.ChildAdded:Wait();
- 
-       local container = Instance.new("Folder");
-       for _, child in next, instance:GetChildren() do
-           child.Parent = container;
+if game.PlaceId == 292439477 then
+    local old; 
+    old = hookmetamethod(runService.Heartbeat, "__index", function(_, index)
+        return old(_, index == "ConnectParallel" and "Connect" or index);
+    end);
+        
+    replicatedFirst.ChildAdded:Connect(function(instance)
+       if instance:IsA("Actor") then
+           replicatedFirst.ChildAdded:Wait();
+     
+           local container = Instance.new("Folder");
+           for _, child in next, instance:GetChildren() do
+               child.Parent = container;
+           end
+     
+           container.Name = instance.Name;
+           container.Parent = instance.Parent;
+     
+           instance.Parent = nil;
        end
- 
-       container.Name = instance.Name;
-       container.Parent = instance.Parent;
- 
-       instance.Parent = nil;
-   end
-end);
+    end);
+    
+end
